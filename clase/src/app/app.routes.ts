5d90bf3e331d1f1da07637567f6071bbe178f985
Mau Torrecillas <maudy_torrecillasnu@fet.edu.co>
2025-09-11 17:11:26 -0500

    ww

--- clase/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { ComponenteUnoComponent } from './componente-uno/componente-uno.component';
-
-export const routes: Routes = [{
-    path: 'Manwy',
-    component: ComponenteUnoComponent
-}];
